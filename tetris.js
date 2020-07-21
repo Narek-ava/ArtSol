8695c80f520cbd5bc1ee706d7fedf08b15afa82b
Narek <58231869+Narek-ava@users.noreply.github.com>
2020-07-21 21:14:01 +0400

    aa

--- tetris.js
+++ tetris.js
@@ -1,7 +1,7 @@
 window.onload = main;
 
 const M = 10;
-const N = 5;
+const N = 4;
 let matrix = [];
 let randomJ = Math.floor(Math.random() * N);
 let randomJ2 = Math.floor(Math.random() * N);
@@ -9,32 +9,33 @@ let wallI = 0;
 let wallI2 = 0;
 let carCurrentJ = 2;
 let algorithm;
-let interval = 150;
+let interval = 90;
 let carCount = 0;
 let keydownCount = 0;
 let highScore = [0];
-console.log(randomJ);
+let coordinates = M - 1;
 
 function main() {
+
     createMatrix();
     setCar();
-    console.log(matrix);
-        document.addEventListener("keydown", (e) => {
 
-            if (e.code === 'Space') {
-                algorithm = setInterval(() => {
-                   wallI = startAlgorithm(wallI,randomJ);
-                   if(wallI > randomJ || wallI2 > 0){
-                       wallI2 = startAlgorithm(wallI2,randomJ2);
+    document.addEventListener("keydown", (e) => {
 
-                   }
-                   
-                    if (isFinished()) {
-                        stopGame();
-                    }
-                }, interval);
-            }
-        });
+        if (e.code === 'Space') {
+            algorithm = setInterval(() => {
+                wallI = startAlgorithm(wallI, randomJ);
+
+                if (wallI >= randomJ2 || wallI2 > 0) {
+                    wallI2 = startAlgorithm(wallI2, randomJ2);
+                }
+
+                if (isFinished()) {
+                    stopGame();
+                }
+            }, interval);
+        }
+    });
 
 
     document.addEventListener("keydown", function (press) {
@@ -61,16 +62,12 @@ function createMatrix() {
             matrix[i][j] = 0;
             let cell = document.createElement('td');
             cell.id = 'cell_' + i + '_' + j;
-            // if (j === 2) {
-            //     document.getElementById(cell.id).classList.add("road2");
-            //     // console.log(cell.id);
-            // }
             row.appendChild(cell);
         }
     }
 }
 
-function startAlgorithm(wallI,randomJ) {
+function startAlgorithm(wallI, randomJ) {
     let back = wallI - 1;
     let cell = 'cell_' + wallI + '_' + randomJ;
     let cellBack = 'cell_' + back + '_' + randomJ;
@@ -97,80 +94,86 @@ function startAlgorithm(wallI,randomJ) {
         document.getElementById("score").innerHTML = "Score" + " " + carCount;
 
     }
+
     return wallI;
+
 }
 
 function moveCarLeft() {
     let leftMove = carCurrentJ - 1;
-    let left = 'cell_' + 9 + '_' + leftMove;
-    let rightDel = 'cell_' + 9 + '_' + carCurrentJ;
-    document.getElementById(left).classList.add('car');
+    let left = 'cell_' + coordinates + '_' + leftMove;
+    let rightDel = 'cell_' + coordinates + '_' + carCurrentJ;
 
+    document.getElementById(left).classList.add('car');
     document.getElementById(rightDel).classList.remove('car');
-    matrix[M - 1][leftMove] = 1;
-    matrix[M - 1][carCurrentJ] = 0;
+    matrix[coordinates][leftMove] = 1;
+    matrix[coordinates][carCurrentJ] = 0;
     carCurrentJ = leftMove;
-    // console.log(matrix);
 }
 
 function moveCarRight() {
     let rightMove = carCurrentJ + 1;
-    let right = 'cell_' + 9 + '_' + rightMove;
-    let leftDel = 'cell_' + 9 + '_' + carCurrentJ;
+    let right = 'cell_' + coordinates + '_' + rightMove;
+    let leftDel = 'cell_' + coordinates + '_' + carCurrentJ;
     document.getElementById(right).classList.add('car');
-    //console.log(matrix)
     document.getElementById(leftDel).classList.remove('car');
-    matrix[M - 1][rightMove] = 1;
-    matrix[M - 1][carCurrentJ] = 0;
+    matrix[coordinates][rightMove] = 1;
+    matrix[coordinates][carCurrentJ] = 0;
     carCurrentJ = rightMove;
-    //  console.log(matrix);
 }
 
-
 function isOnMatrixLeft(carCurrentJ) {
-
-    return carCurrentJ >= 0 && carCurrentJ < 10 && matrix[M - 1][carCurrentJ - 1] === 0;
+    return carCurrentJ >= 0 && carCurrentJ < 10 && matrix[coordinates][carCurrentJ - 1] === 0;
 }
 
 function isOnMatrixRight(carCurrentJ) {
-
-    return carCurrentJ >= 0 && carCurrentJ < 10 && matrix[M - 1][carCurrentJ + 1] === 0;
+    return carCurrentJ >= 0 && carCurrentJ < 10 && matrix[coordinates][carCurrentJ + 1] === 0;
 }
 
 function isFinished() {
-
     let arr = [];
+
     for (let j = 0; j < N; j++) {
-        if (matrix[ M - 1][j] === 0)
+
+        if (matrix[coordinates][j] === 0)
             arr.push(0);
     }
+
     if (arr.length === N) {
         return true;
     }
 }
 
 function randomNum(random) {
-    if (random === randomJ){
+
+    if (random === randomJ) {
         return randomJ = Math.floor(Math.random() * N);
     }
-    if (random === randomJ2){
-        return randomJ2 =  Math.floor(Math.random() * N);
-    }
 
+    if (random === randomJ2) {
+        return randomJ2 = Math.floor(Math.random() * N);
+    }
 }
 
 function setCar() {
-    matrix [M - 1][carCurrentJ] = 1;
+
+    matrix [coordinates][carCurrentJ] = 1;
+
     document.getElementById("cell_9_2").classList.add('car')
 }
 
 function stopGame() {
+    let carLastPictureId = "cell_" + coordinates + "_" + carCurrentJ;
     clearInterval(algorithm);
+
     if (carCount > highScore[0]) {
         highScore[0] = carCount;
         document.getElementById("highScore").innerHTML = "HighScore" + " " + highScore[0];
 
     }
+
+    document.getElementById(carLastPictureId).classList.remove('car');
     keydownCount = 0;
     carCount = 0;
+    setCar();
 }
